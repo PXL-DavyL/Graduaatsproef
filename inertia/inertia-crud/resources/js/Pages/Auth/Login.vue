48f890aef3cb69808047b91a8365c428a53807b7
Dignity <mionedur4@gmail.com>
2025-03-12 13:36:21 +0100

    feat: adding 'register' hotlink to login page + linting

--- inertia/inertia-crud/resources/js/Pages/Auth/Login.vue
+++ inertia/inertia-crud/resources/js/Pages/Auth/Login.vue
@@ -1,32 +1,3 @@
-<script setup>
-import GuestLayout from '@/Layouts/GuestLayout.vue';
-import InputCheck from '@/Components/InputCheck.vue';
-import InputButton from '@/Components/InputButton.vue';
-import InputText from '@/Components/InputText.vue';
-import { Head, Link, useForm } from '@inertiajs/vue3';
-
-defineProps({
-    canResetPassword: {
-        type: Boolean,
-    },
-    status: {
-        type: String,
-    },
-});
-
-const form = useForm({
-    email: '',
-    password: '',
-    remember: false,
-});
-
-const submit = () => {
-    form.post(route('login'), {
-        onFinish: () => form.reset('password'),
-    });
-};
-</script>
-
 <template>
     <GuestLayout>
         <Head title="Log in" />
@@ -47,7 +18,7 @@ const submit = () => {
                     autofocus
                     autocomplete="username"
                     :error="form.errors.email"
-                />    
+                />
             </div>
 
             <div class="mt-4">
@@ -60,11 +31,15 @@ const submit = () => {
                     required
                     autocomplete="current-password"
                     :error="form.errors.password"
-                />    
+                />
             </div>
 
             <div class="mt-4 block">
-                <InputCheck text="Remember me" name="remember" v-model:checked="form.remember" />
+                <InputCheck
+                    text="Remember me"
+                    name="remember"
+                    v-model:checked="form.remember"
+                />
             </div>
 
             <div class="mt-4 flex items-center justify-end">
@@ -84,6 +59,48 @@ const submit = () => {
                     Log in
                 </InputButton>
             </div>
+
+            <div>
+                <Link
+                    v-if="canRegister"
+                    :href="route('register')"
+                    class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                    Create an account
+                </Link>
+            </div>
         </form>
     </GuestLayout>
 </template>
+
+<script setup>
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+import InputCheck from "@/Components/InputCheck.vue";
+import InputButton from "@/Components/InputButton.vue";
+import InputText from "@/Components/InputText.vue";
+import { Head, Link, useForm } from "@inertiajs/vue3";
+
+defineProps({
+    canResetPassword: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    status: {
+        type: String,
+    },
+});
+
+const form = useForm({
+    email: "",
+    password: "",
+    remember: false,
+});
+
+const submit = () => {
+    form.post(route("login"), {
+        onFinish: () => form.reset("password"),
+    });
+};
+</script>
