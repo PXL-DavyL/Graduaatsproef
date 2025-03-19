d3e560aefa4d2a568fdc24dcd9ea60d8fa65d7bd
Dignity <mionedur4@gmail.com>
2025-03-19 10:51:46 +0100

    refactor: admin user index update, add create button, add hint, linting

--- inertia/inertia-crud/resources/js/Pages/Admin/User/List.vue
+++ inertia/inertia-crud/resources/js/Pages/Admin/User/List.vue
@@ -1,17 +1,21 @@
 <template>
     <AdminLayout title="Manage Users">
         <div class="flex flex-col gap-4">
-            <span class="text-2xl font-bold">Users</span>
-
             <div class="mt-3 flex flex-col gap-5">
                 <div class="flex flex-col border border-zinc-600 rounded">
-                    <span class="p-2 bg-zinc-700"> Users </span>
+                    <div class="p-2 bg-zinc-700 flex justify-between">
+                        <span> User List </span>
+                        <InputButtonLink
+                            type="primary"
+                            :href="route('admin.users.create')"
+                        >
+                            Create New User
+                        </InputButtonLink>
+                    </div>
                     <table class="table-auto w-full text-left border-collapse">
                         <thead>
                             <tr class="bg-zinc-600 text-white">
-                                <th class="p-2 border-b border-zinc-500">
-                                    ID
-                                </th>
+                                <th class="p-2 border-b border-zinc-500">ID</th>
                                 <th class="p-2 border-b border-zinc-500">
                                     Name
                                 </th>
@@ -51,7 +55,7 @@
                             </tr>
                         </tbody>
                     </table>
-                    <span class="p-2 bg-zinc-600"> Browse all users </span>
+                    <span class="p-2 bg-zinc-600"> Click on a row to edit or delete the user. </span>
                 </div>
             </div>
 
@@ -62,10 +66,12 @@
 <script setup>
 import { usePage } from "@inertiajs/vue3";
 import { formatDate } from "@/Composables/dates";
+
 import Pagination from "@/Components/Pagination.vue";
 import AdminLayout from "@/Layouts/AdminLayout.vue";
+import InputButtonLink from "@/Components/InputButtonLink.vue";
 
 const onClickRow = (user) => {
-    window.location.href = route('admin.users.edit', user);
-}
+    window.location.href = route("admin.users.edit", user);
+};
 </script>
